9a042d6bb34b6f18b41d8db925e2dbca452dad5c
Conor Koritor <ckoritor@gmail.com>
2023-11-30 12:56:03 +0000

    Created Team and Player Class

---
+++ ConorKoritorOOPCA2/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConorKoritorOOPCA2
+{
+    internal class Player
+    {
+        private string Name {  get; set; }
+        private List<string> ResultRecord {  get; set; }
+
+        public Player()
+        {
+            //Constructor with no Arguments
+            //Will not need as we will hard code the players. Just future proofs the code
+            Name = "No Name";
+            ResultRecord = new List<string>{"L", "L", "L", "L", "L"};
+        }
+
+        public Player(string name, List<string> resultRecord)
+        {
+            Name = name;
+            ResultRecord = resultRecord;
+        }
+    }
+}
---
+++ ConorKoritorOOPCA2/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConorKoritorOOPCA2
+{
+    internal class Team
+    {
+        private string Name {  get; set; }
+        private ObservableCollection<Player> Players { get; set; }
+
+        public Team() 
+        {
+            //Constructor if no data is entered
+            //Unnecessary as we will hard code the teams but just future proofs the code
+            Name = "No Name";
+            Players = new ObservableCollection<Player>();
+        }
+
+        public Team(string name, ObservableCollection<Player> players)
+        {
+            Name = name;
+            Players = players;
+        }
+    }
+}
